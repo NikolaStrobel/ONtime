e9604b51fd3e11c3f96fac9258bfb326eaa7c4a0
Aaron <aaron.chua@hotmail.com>
2019-10-12 13:06:25 +0800

    change a little

--- ONtime/public_html/js/productReview.js
+++ ONtime/public_html/js/productReview.js
@@ -1,9 +1,12 @@
 
 function validateform() {
-    var review = document.forms["productReview"]["review"].value;
+    var review = document.forms["productreview"]["review"].value;
 
       if (review == "") {
         alert("Form must be filled out");
         return false;
       }
+      else
+        alert("Form Submitted Successfully");
+        return false;
     }
